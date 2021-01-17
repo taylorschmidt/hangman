14a45385dc51ded9b7fc7b221db94e9c8d83c906
taylorschmidt <taylorhillschmidt@gmail.com>
2021-01-16 17:05:10 -0700

    created init method

--- hangmanpy.py
+++ hangmanpy.py
@@ -1,18 +1,28 @@
 class Word():
     def __init__(self, chosen_word):
+
+        # this method will split the word up into a list of dictionaries with 2 attributes:
+        
+        # the letter/character, and a boolean representing whether or not it has been guessed
+
         self.chosen_word = chosen_word
         print(chosen_word)
-        current = list('_' * len(chosen_word))
+        current = '_ ' * len(chosen_word)
         print(current)
+        # initialize game list
+        game_list = []
+        # loop through chosen word
         for char in chosen_word:
+            # for each character, create a dictionary with the letter and false guess
             d = dict(char= char, guessed=False)
-            print(d)
+            # add that dictionary to the game list
+            game_list.append(d)
+
+        print(game_list)
 
 
     
-# this method will split the word up into a list of dictionaries with 2 attributes:
-        
-# the letter/character, and a boolean representing whether or not it has been guessed
+
 
 Word('hangman')
     
