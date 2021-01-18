40b23277509a0a4957dea0eeb45f0c76b9fead55
taylorschmidt <taylorhillschmidt@gmail.com>
2021-01-18 16:15:58 -0700

    added check end game functions

--- hangmanpy.py
+++ hangmanpy.py
@@ -2,18 +2,13 @@ class Word():
     def __init__(self, chosen_word):
         self.chosen_word = chosen_word
         self.game_list = []
-        print(chosen_word)
-        current = '-' * len(chosen_word)
-        print(current)
+        self.game_over = False
         # loop through chosen word
         for char in chosen_word:
             # for each character, create a dictionary with the letter and false guess
             d = dict(char= char, guessed=False)
             # add that dictionary to the game list
             self.game_list.append(d)
-
-        print(self.game_list)
-        self.start_game()
 
     def game_status(self):
         #initiate the game status
@@ -29,46 +24,49 @@ class Word():
                 status += "-"
         # print the status
         print(status) 
-
+        self.game_end()
 
     def start_game(self):
-        self.guess = input("Guess a letter: ").lower()
-        print(self.guess)
-        # loop through the game list
-        for dic in self.game_list:
-            if dic['char'] == self.guess:
-                dic['guessed'] = True
-                print(self.game_list)
-            elif dic['char'] != self.guess:
-                continue
+        print('You are now playing Hangman! Press a key to start.')
+        input()
+        print('Here is your current word\'s length:')
         self.game_status()
-        self.check_end()
+        self.game_play()
+
+    def game_play(self):
+        if self.game_over == False:
+            self.guess = input("Guess a letter: ").lower()
+            print(self.guess)
+            # loop through the game list
+            for dic in self.game_list:
+                if dic['char'] == self.guess:
+                    dic['guessed'] = True
+                elif dic['char'] != self.guess:
+                    continue
+            self.game_status()
+            # self.check_end()
+            self.game_play()
 
     def check_end(self):
         for dic in self.game_list:
             if dic['guessed'] == False:
                 return False
-            return True
-        print("Checked for end game.")
+        return True
 
 
-        # if guessed is false, print a '-' at that location
-        # if guessed is true, print the letter at that location
-
-    # def get_word():
-
-    # def show_scoreboard():
-
-    # def process_user_guesses(): #perhaps?
+    def game_end(self):
+        if self.check_end():
+            self.game_over = True
+            print('Congratulations, you win!')
+        else:
+            return
 
 
-# some variables here to prepare the wordlist, initialize things like
 # `remaining_guesses` (start a round with 8), `letters_used`, the `chosen_word` (randomly
-# chosen from a list of words you also declare here perhaps?),
-#and whatever else you might want to keep track of
+# chosen from a list of words you also declare
 
     
 
-
-Word('hangman')
+game = Word('hangman')
+game.start_game()
     
